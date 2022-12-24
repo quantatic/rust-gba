4a664520083179182275fa344427a30752316a46
Aidan Beggs <beggsa@amazon.com>
2022-12-24 22:30:07 +0000

    Experimental "b" impl.

--- emulator-core/src/cpu/jit.rs
+++ emulator-core/src/cpu/jit.rs
@@ -23,27 +23,29 @@ impl Cpu {
     pub fn try_jit(instruction: ArmInstruction) -> Option<JitInstruction> {
         let mut assembler: Assembler<X64Relocation> = Assembler::new().unwrap();
 
+        let start = assembler.offset();
         dynasm!(assembler
             ; .arch x64
-            ; int3
+            // ; int3
             ; push rbp
             ; mov rbp, rsp
             ; push rdi
+            ; sub rsp, 8
         );
-        let start = assembler.offset();
 
         match instruction.instruction_type() {
             ArmInstructionType::B { offset } => {
                 dynasm!(assembler
-                    ; mov rdi, [rsp]
-                    ; mov sil, Register::R15 as _
-                    ; call Self::jit_read_register as _
+                    ; mov rdi, [rbp - 8]
+                    ; mov rsi, Register::R15 as _
+                    ; mov rax, QWORD Self::jit_read_register as i64
+                    ; call rax
                     ; add eax, offset
-                    ; mov rdi, [rsp]
+                    ; mov rdi, [rbp - 8]
                     ; mov esi, eax
-                    ; mov dl, Register::R15 as _
-                    ; call Self::jit_write_register as _
-                    ; ret
+                    ; mov rdx, Register::R15 as _
+                    ; mov rax, QWORD Self::jit_write_register as i64
+                    ; call rax
                 );
             }
             _ => return None,
@@ -52,6 +54,7 @@ impl Cpu {
         dynasm!(assembler
             ; mov rsp, rbp
             ; pop rbp
+            ; ret
         );
 
         let buffer = assembler.finalize().unwrap();
@@ -61,10 +64,13 @@ impl Cpu {
     }
 
     extern "sysv64" fn jit_read_register(&self, register: Register) -> u32 {
-        self.read_register(register, |_| unreachable!())
+        self.read_register(register, |pc| pc)
     }
 
     extern "sysv64" fn jit_write_register(&mut self, value: u32, register: Register) {
+        println!("value: {:08X}", value);
+        println!("register: {:?}", register);
         self.write_register(value, register);
+        println!("done writing register");
     }
 }
